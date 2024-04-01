872aaa8a4d024f29c22a4e932066950e545b5143
Asim307 <asimshaikhby0097@gmail.com>
2024-04-01 06:03:02 -0700

    this is simple todo list

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let add = await inquirer.prompt([
+        { message: "what you want to add in your todo list", type: "input", name: "todo" },
+        { message: "do you add more items in your todo list", type: "confirm", name: "addmore", default: "false" },
+    ]);
+    todos.push(add.todo);
+    condition = add.addmore;
+    console.log(todos);
+}
